784e92f98b56f9db012bc3a3ad7e8d0f275a3806
Nadia Garnou <garnou41@gmail.com>
2020-11-16 23:45:16 +0100

    Correction de la fonction mycat

--- Code/functions.h
+++ Code/functions.h
@@ -7,55 +7,45 @@
 #include "tar.h"
 #include <string.h>
 #include <stdlib.h>
+/********split*****************/
+/**verifie si le chemin contient un tar ,si oui elle renvoie le chemin du tar et celui du fichier  *******/
+char *split(char *path,int num){
+	char *tar=strdup(path),*filename=NULL;
+	int i=0,n;
+	while(i!=-1){
+			if(path[i]=='.' && path[i+1]=='t' && path[i+2]=='a' && path[i+3]=='r'){
+				tar[i+4]='\0';
+				i=-1;
+			}
+			else{
+				i++;
+			}
+		}
+		if(strlen(path)-strlen(tar)==0)
+			return "echec";
+		filename=malloc(strlen(path)-strlen(tar));
+		strncat(filename,path+strlen(tar)+1,strlen(path)-strlen(tar)-1);
+	if(num==1)
+		return tar;
+	if(num==2)
+		return filename;
+	if(strlen(path)-strlen(tar)==0)
+		return "echec";
+	return "";
+	}	
 
 
 /***********compare deux chaînes de caractéres**************/
 
 int cmp(char *name1,char *name2){
+	if(strlen(name1)!=strlen(name2))
+		return -1;
 	for(int i=0;i<strlen(name1);i++){
 		if(name1[i]!=name2[i])
 			return -1;
 		}
 	return 0;
 	}
-/************récupére le contenu d'un fichier tar********/
-
-int contenu_tar(char *chemin,char *filename){
-	struct posix_header p;
-	char buffer[512];
-	char tmp[1024];
-	int fd=open(chemin,O_RDONLY),j,i=0,lus;
-	if(fd==-1)
-		perror("le repertoire n'existe pas");
-		
-	do {
-		lus=read(fd,&p,512);
-	}	
-	while(cmp(p.name,filename)==-1 && lus>0);
-		
-	if(cmp(p.name,filename)==0){
-			if(((atoi(p.size)-512+1)/512) <1 )
-			{
-				lus=read(fd,tmp,atoi(p.size));
-				write(STDOUT_FILENO ,tmp,lus);
-			  }
-			else
-			{	
-				while(j<((atoi(p.size)-512+1)/512)-2){	
-					lus=read(fd,buffer,512);
-					write(STDOUT_FILENO ,buffer,lus);
-					j++;
-					}
-				}
-		 close(fd);
-		 
-		 return 0;
-		 
-			}
-	close(fd);
-	return -1;
-
-}
 /***************vérifie si c'est une archive tar********************/
 
 int isTar (char c[]){
--- Code/mycat.c
+++ Code/mycat.c
@@ -1,4 +1,3 @@
-#include <sys/types.h>
 #include <errno.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,7 +6,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <dirent.h>
-#include <tar.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "functions.h"
 
 
@@ -15,8 +15,7 @@
 int contenu_tar(char *chemin,char *filename){
 	struct posix_header p;
 	char buffer[512];
-	char tmp[1024];
-	int fd=open(chemin,O_RDONLY),j,i=0,lus;
+	int fd=open(chemin,O_RDONLY),j,i=0,lus,size;
 	if(fd==-1){
 		perror("le repertoire n'existe pas");
 		exit(EXIT_FAILURE);
@@ -27,94 +26,57 @@ int contenu_tar(char *chemin,char *filename){
 		while(cmp(p.name,filename)==-1 && lus>0);
 			
 		if(cmp(p.name,filename)==0){
-				if(((atoi(p.size)-512+1)/512)<=1)
-				{
-					lus=read(fd,tmp,atoi(p.size));
-					write(STDOUT_FILENO ,tmp,lus);
-				  }
-				else
-				{	
-					while(j<((atoi(p.size)-512+1)/512)-1 && lus>0){	
-						lus=read(fd,buffer,512);
-						write(STDOUT_FILENO ,buffer,lus);
-						j++;
-						}
-					}
-			 close(fd);
-			 
+			sscanf(p.size,"%o",&size);
+			for(int i=0;i<((size+512-1)/512);i++){
+				lus=read(fd,buffer,512);
+				write(STDOUT_FILENO,buffer,lus);
+			}	
+				close(fd);
 			 return 0;
 			 
 				}
 		close(fd);
+		perror("Aucun fichier de ce type");
 		return -1;
 }
-/********split*****************/
-/**verifie si le chemin contient un tar ,si oui elle renvoie le chemin du tar et celui du fichier  *******/
-char *split(char *path,int num){
-	char *tar=strdup(path),*filename=NULL;
-	int i=0,n;
-	while(i!=-1){
-			if(path[i]=='.' && path[i+1]=='t' && path[i+2]=='a' && path[i+3]=='r'){
-				tar[i+4]='\0';
-				i=-1;
-			}
-			else{
-				i++;
-			}
-		}
-		if(strlen(path)-strlen(tar)==0)
-			return "echec";
-		filename=malloc(strlen(path)-strlen(tar));
-		strncat(filename,path+strlen(tar)+1,strlen(path)-strlen(tar)-1);
-	if(num==1)
-		return tar;
-	if(num==2)
-		return filename;
-	if(strlen(path)-strlen(tar)==0)
-		return "echec";
-	return "";
-	}	
 
 
 /***************la fonction cat*****************/
 	
 int main(int argc, char **argv){
-
-	int fd;
-	int lus;
-	char buffer[1000];
-	
-	if(argc<2){
-		while(lus =read(0, buffer ,sizeof(buffer)))
-			write(1 ,buffer ,lus);
+	int i=1,option=-1;
+	//verifie si l'un des arguments est une option
+	while(i<argc){
+		if(argv[i][0]=='-' ){
+				option=0;	
+					}
+			i++;
+		}
+	if(option==0){
+		execvp("/bin/cat",argv);
 	}
+	
 	else{
 			
-		for(int i=1;i<argc;i++){
-			
+		for(int i=1;i<argc;i++){	
+			//si le chemin du fichier contient un tarball on utilise la fonction contenu_tar
 			if(cmp(split(argv[i],1),"echec")==-1 && cmp(split(argv[i],2),"echec")==-1)
-			{
-				contenu_tar(split(argv[i],1),split(argv[i],2));
-				}
-				
-			if(cmp(split(argv[i],1),"echec")==0)
-			{
-				fd=open(argv[i], O_RDONLY);
-			
-				if(fd == -1)
-					perror("Cannot open file");
-					
-				else{
-					
-					while(lus =read(fd, buffer ,sizeof(buffer)))
-						write(STDOUT_FILENO ,buffer ,lus);
-						
-					close(fd);
-					}
-			}
+					{
+						contenu_tar(split(argv[i],1),split(argv[i],2));
+						}
+					else{
+						//sinon on execute la commande cat usuelle
+						int pid=fork();
+						if(pid<0)
+							return (EXIT_FAILURE);
+						if(pid==0){
+								execlp("cat","cat",argv[i],NULL);
+							}		
+							waitpid(pid,NULL,0);
+							}
+						}	
 		}
-		
-	}
 	return 0;
 
 }
+
