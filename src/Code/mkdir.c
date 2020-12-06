438774d591b70c637efefd837936027bdd7aa061
Tanel Oubadia <tobitreex@gmail.com>
2020-12-06 16:40:53 +0100

    starting mkdir

---
+++ src/Code/mkdir.c
@@ -0,0 +1,15 @@
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "functions.h"
+
+int tar_creation(){
+  
+}
