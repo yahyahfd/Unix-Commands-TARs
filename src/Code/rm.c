269f9008548b0c81f28436f083a9852221787824
Tanel Oubadia <tobitreex@gmail.com>
2020-12-07 13:17:56 +0100

    not working yet

--- src/Code/rm.c
+++ src/Code/rm.c
@@ -10,6 +10,24 @@
 #include <sys/wait.h>
 #include "functions.h"
 
+int occurences(char *filepath){
+	struct posix_header p;
+	static int k;
+	int fd=open(split(filepath,1),O_RDWR);
+	if(fd==-1){
+		perror("error while opening the file");
+		exit(-1);
+		}
+	k=0;
+	while(read(fd,&p,512)>0 && p.name[0]!='\0'){
+		if(cmp(split(filepath,2),p.name)==0){
+				k+=1;
+				}
+	}
+		return k;
+
+	}
+
 int tar_remove(char *filepath,int opt){
 
   int fd=open(split(filepath,1),O_RDWR);
@@ -34,49 +52,48 @@ int tar_remove(char *filepath,int opt){
   struct posix_header p;
   char *filename = split(filepath,2);
   int lus,size=0;
-  int static i=0;
+  int i=0,j=0;
 
   while(lus=read(fd,&p,sizeof(struct posix_header))>0){
     i+=512;
+    j+=512;
     if(cmp(filename,p.name)==0){
-      if(p.typeflag=='5' && opt==1){
-        perror("You cannot remove a non-empty directory without the -r option");
-        exit(EXIT_FAILURE);
+      if(occurences(filepath)==0){
+        write(STDOUT_FILENO, "rmdir: No such file or directory\n", strlen("rmdir: No such file or directory\n"));
+       exit(EXIT_FAILURE);
       }
-      lus=read(fd,&p,sizeof(struct posix_header));
-      if(strcmp(filename,p.name)!=0 && strlen(filename)==strlen(p.name)){
-        i+=512;
-      }
-      if(strcmp(filename,p.name)==0 && strlen(filename)!=strlen(p.name)){
-        perror("This directory is not empty");
-        exit(EXIT_FAILURE);
-      }else{
-        lseek(fd,i,SEEK_SET);
-        i-=512;
-        while(lus=read(fd,&p,sizeof(struct posix_header))>0){
-          lseek(fd,i,SEEK_SET);
-          i+=512;
-          write(fd,&p,sizeof(struct posix_header));
-          p.name[0]='\0';
-          sscanf(p.size,"%o",&size);
-          while(size>0){
-            read(fd,&p,sizeof(struct posix_header));
-            lseek(fd,i,SEEK_SET);
-            i+=size;
-            size-=512;
-            write(fd,&p,size);
-          }
-        }
-        exit(EXIT_SUCCESS);
-      }
-      if(p.typeflag!='5'){
-        sscanf(p.size,"%o",&size);
-        i+=size;
-      }
+      if(occurences(filepath)>1 && opt == 1){
+        write(STDOUT_FILENO, "rm: Cannot delete a non-empty directory without the -r option\n", strlen("rm: Cannot delete a non-empty directory without the -r option\n"));
+       exit(EXIT_FAILURE);
+     }else{
+       lseek(fd,i,SEEK_SET);
+       read(fd,&p,512);
+       if(	p.name[0]=='\0'){
+         memset(&p,'\0',512);
+         lseek(fd,i-512,SEEK_SET);
+         write(fd,&p,512);
+         set_checksum(&p);
+         exit(0);
+       }else{
+         lseek(fd,i,SEEK_SET);
+         while((lus=read(fd,&p,512))>0 && p.name[0]!='\0'){
+           lseek(fd,i-512,SEEK_SET);
+           i+=512;
+           write(fd,&p,lus);
+           lseek(fd,512,SEEK_CUR);
+           j+=512;
+         }
+         memset(&p,'\0',512);
+         lseek(fd,j-512,SEEK_SET);
+         write(fd,&p,512);
+         set_checksum(&p);
+         exit(0);
+       }
+     }
+
     }
     lseek(fd,i,SEEK_SET);
   }
-  exit(EXIT_FAILURE);
 
   return 0;
 }
